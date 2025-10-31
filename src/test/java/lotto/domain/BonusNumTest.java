5b9fa767df720d3acd0615841780f6b3f9f2b0fe
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-31 15:48:55 +0900

    test(BonusNumTest): 보너스 번호 검증 테스트

---
+++ src/test/java/lotto/domain/BonusNumTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BonusNumTest {
+
+    @DisplayName("보너스 번호 범위가 1~45가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 50})
+    void bonusRange_test(int outRangeNum) {
+        assertThatThrownBy(() -> new BonusNum(outRangeNum, new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이 숫자여야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 겹칠 시 예외가 발생한다.")
+    @Test
+    void validateDuplicate_test() {
+        assertThatThrownBy(() -> new BonusNum(1, new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 겹쳐서는 안 됩니다.");
+    }
+}
