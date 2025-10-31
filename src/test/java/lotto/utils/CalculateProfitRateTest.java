cdd5591a8aa431676b65ceb2e8178e97f2446df3
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-31 18:32:00 +0900

    test(CalculateProfitRateTest): 수익률 계산 테스트

---
+++ src/test/java/lotto/utils/CalculateProfitRateTest.java
@@ -0,0 +1,31 @@
+package lotto.utils;
+
+import lotto.domain.enums.LottoRank;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculateProfitRateTest {
+
+    EnumMap<LottoRank, Integer> rankResult = new EnumMap<>(LottoRank.class);
+
+    @BeforeEach
+    void setup() {
+        for (LottoRank rank : LottoRank.values()) {
+            rankResult.put(rank, 0);
+        }
+        rankResult.put(LottoRank.FIFTH, rankResult.get(LottoRank.FIRST) + 1);
+    }
+
+    @DisplayName("수익률 계산을 테스트")
+    @Test
+    void calculateProfitRate_test() {
+        double profitRate = CalculateProfitRate.calculate(rankResult, 3);
+        assertThat(profitRate).isEqualTo(166.67);
+    }
+
+}
