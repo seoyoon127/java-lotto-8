5ac726fb5e51489420abb81389e0bdba89904a6e
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-31 15:39:32 +0900

    test(LottoCostTest): 구매 금액 검증 테스트

---
+++ src/test/java/lotto/domain/LottoCostTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoCostTest {
+
+    @DisplayName("로또 금액이 양수가 아닐 시 예외가 발생한다.")
+    @Test
+    void validateIsPositive_test() {
+        assertThatThrownBy(() -> new LottoCost(-12000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 양수여야 합니다.");
+    }
+
+    @DisplayName("로또 금액이 1000원 단위가 아닐 시 예외가 발생한다.")
+    @Test
+    void validateUnit_test() {
+        assertThatThrownBy(() -> new LottoCost(12345))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매 금액은 1000 단위여야 합니다.");
+    }
+}
